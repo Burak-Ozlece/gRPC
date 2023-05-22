38f186c2d10a860c8b1bfc747b8e5f1fd5780cbd
Burak Özlece <burakozleceplus@gmail.com>
2023-05-23 00:28:32 +0300

    Add project files.

---
+++ grpcClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using Google.Protobuf;
+using Grpc.Net.Client;
+using grpcFileTransportClient;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7062");
+var client = new FileService.FileServiceClient(channel);
+
+string file = @"C:\\Users\\burak\\Videos\\2023-05-06 21-56-20.mkv";
+
+FileStream fileStream = new(file, FileMode.Open);
+
+var content = new BytesContent
+{
+    FileSize = fileStream.Length,
+    ReadedByte = 0,
+    Info = new grpcFileTransportClient.FileInfo
+    {
+        FileName = Path.GetFileNameWithoutExtension(fileStream.Name),
+        FileExtension = Path.GetExtension(fileStream.Name)
+    }
+};
+
+var upload = client.FileUpload();
+byte[] buffer = new byte[2048];
+while ((content.ReadedByte = await fileStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+{
+    content.Buffer = ByteString.CopyFrom(buffer);
+    await upload.RequestStream.WriteAsync(content);
+}
+
+await upload.RequestStream.CompleteAsync();
+fileStream.Close();
